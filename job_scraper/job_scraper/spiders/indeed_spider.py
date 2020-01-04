90ab0230a8236ef41de1dbf576ef8562d69c493a
danwan754 <danwan754@gmail.com>
2020-01-03 18:57:29 -0800

    Add xpath expressions for title, company

---
+++ job_scraper/job_scraper/spiders/indeed_spider.py
@@ -0,0 +1,25 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "indeed"
+
+    start_urls = [
+        'https://ca.indeed.com/jobs?q=developer&l=Vancouver%2C+BC&fromage=last'
+    ]
+
+    def parse(self, response):
+
+        posts = response.xpath("//div[contains(@class, 'jobsearch-SerpJobCard') and contains(@class, 'unifiedRow')]")
+
+        for post in posts:
+            title = post.xpath('./div[contains(@class, "title")]/a/@title').get()
+            unstripped_company = post.xpath('./div[contains(@class, "sjcl")]/div/span[contains(@class, "company")]//text()').getall()
+            company = ''.join(unstripped_company).strip()
+            yield {
+                'title': title,
+                'company': company
+                # 'location':
+                # 'date': 
+            } 
+            
