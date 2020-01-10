94687feafee097f0e504b91a88ed939f8bee692c
danwan754 <danwan754@gmail.com>
2020-01-10 13:27:37 -0800

    Append recent IDs to history after emailing posts

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ Email a list of new job posts from Indeed. Applies filters to results.
 '''
 
 recent_scraped_jobs_path = './job_scraper/indeed.json'
+history_ids_path = 'indeed_ids_history.csv'
 post_list = []
 
 # load the JSON from recently scraped jobs
@@ -28,4 +29,10 @@ with open(config_file_path, 'r') as json_file:
 
 # send email
 email_sender.send_email(config_dict, post_list)
+
+# update post ID history with IDs of posts that were just emailed out
+with open(history_ids_path, 'a') as csv_file:
+    writer = csv.writer(csv_file, delimiter=' ')
+    rows_of_post_ids = [[post['id']] for post in post_list]
+    writer.writerows(rows_of_post_ids)
 
