4379fd7d2c158778d4c1451595565d1b3533df4f
Dan Wan <danwan754@gmail.com>
2020-01-15 10:17:37 -0800

    replace file path with <full project path>

--- scrape_indeed.sh
+++ scrape_indeed.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# This script is run by windows task scheduler. The commands below are necessary inorder to successfully scrape Indeed.com and email new job postings.
+# This script is run by windows task scheduler. The commands below are necessary inorder to scrape and email new job postings.
 
-cd /mnt/c/Users/Dan/Projects/python-projs/web-scrapers/job-scraper/job_scraper
-source ../../venv/bin/activate
+cd <full project path>
+source ./venv/bin/activate
 rm -f indeed.json
 scrapy crawl indeed -o indeed.json
 python run.py
