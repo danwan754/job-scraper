aa43db231258b29b13a1e9930ea0e717ebfcbf01
danwan754 <danwan754@gmail.com>
2020-01-31 15:08:30 -0800

    Change to command to python3

--- scrape_indeed.sh
+++ scrape_indeed.sh
@@ -2,8 +2,8 @@
 
 # This script is run by windows task scheduler. The commands below are necessary inorder to scrape and email new job postings.
 
-cd <full project path>
+cd /mnt/c/Users/Dan/Projects/python-projs/web-scrapers/job-scraper/job_scraper
 source ./venv/bin/activate
 rm -f indeed.json
 scrapy crawl indeed -o indeed.json
-python run.py
+python3 run.py
